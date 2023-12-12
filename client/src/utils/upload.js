a006bc353d06e915208b45fbd268d356135840ad
namtp01 <tranphuongnam2812@gmail.com>
2023-12-13 03:08:37 +0700

    abc

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -3,11 +3,12 @@ import axios from "axios";
 const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
-  data.append("upload_preset", "fiverr");
+  data.append("upload_preset", "housekeeper");
 
   try {
-    const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
-
+    //const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
+    const res = await axios.post("https://api.cloudinary.com/v1_1/di6dc8jwa/upload", data);
+    
     const { url } = res.data;
     return url;
   } catch (err) {
