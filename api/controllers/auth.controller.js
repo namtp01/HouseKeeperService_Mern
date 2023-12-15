d0bf647e2368b7023f17f0f5292bba59a55d8188
namtp01 <tranphuongnam2812@gmail.com>
2023-12-15 14:25:48 +0700

    abc

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from '../models/user.model.js'
-import bcrypt from 'bcrypt'
+import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import createError from '../utils/createError.js'
 
 export const register = async (req, res, next) => {
     try {
-        const hash = bcrypt.hashSync(req.body.password, 5)
+        const hash = bcryptjs.hashSync(req.body.password, 5)
         const newUser = new User({
             ...req.body, 
             password: hash
